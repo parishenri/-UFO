5c88e54bc49ce457ca9eaa37bef9b690dafb7674
arthur maisonnier <arthurmaisonnier@gmail.com>
2018-05-22 17:44:04 +0100

    combined navbar and items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'users/:id/dashboard', to: 'pages#dashboard', as: :dashboard
   get 'users/:id/user_profile', to: 'pages#user_profile', as: :user_profile
+  get '/user_listing', to: 'pages#user_listing', as: :user_listing
 
   resources :users, only: [:edit, :update]
 
