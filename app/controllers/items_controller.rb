a0dbf8fdccec9f47d3d515a124db2e1885af0b15
Henri Paris <paris_henri@yahoo.fr>
2018-05-22 15:35:05 +0100

    Controller + view for items done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,47 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to items_path(@item)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def udpate
+  def update
+    @item.update(item_params)
+    redirect_to item_path(@item)
   end
 
   def show
   end
 
   def destroy
+    @item.delete
+    redirect_to items_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :rental_price, :buying_price, :size, :availability, :rental_only)
   end
 end
