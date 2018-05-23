8fa183b45181e39d7ddf52b539aac391c03cc659
arthur maisonnier <arthurmaisonnier@gmail.com>
2018-05-23 14:42:41 +0100

    linked booking and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-
   get 'users/:id/dashboard', to: 'pages#dashboard', as: :dashboard
   get 'users/:id/user_profile', to: 'pages#user_profile', as: :user_profile
   get '/user_listing', to: 'pages#user_listing', as: :user_listing
 
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] do
+    resources :bookings, only: :index
+  end
 
   root to: 'pages#home'
 
   resources :items do
-    resources :bookings
+    resources :bookings, except: :index
   end
 
 end
