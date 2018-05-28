5a6d2ed250f663770f74880983e2d4a159fdc39a
Chris Morrow <chrismorrow@blueyonder.co.uk>
2018-05-28 18:42:06 +0100

    done - needs styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,30 @@ class Item < ApplicationRecord
   validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
   include PgSearch
+
+
   pg_search_scope :global_search,
-    against: [ :name, :description,  ],
+    against: [ :name, :description, :size, :buying_price, :rental_price],
     associated_against: {
       user: [ :address ]
     },
     using: {
       tsearch: { prefix: true }
     }
+
+  def self.filter(args)
+    size = args[:size] if args[:size].present?
+    rental_price = args[:rental_price] if args[:rental_price].present?
+    buying_price = args[:buying_price] if args[:buying_price].present?
+
+    return where(size: size, rental_price: rental_price, buying_price: buying_price) if size && rental_price && buying_price
+    return where(size: size, rental_price: rental_price) if size && rental_price
+    return where(size: size, buying_price: buying_price) if size && buying_price
+    return where(rental_price: rental_price, buying_price: buying_price) if rental_price && buying_price
+    return where(size: size) if size
+    return where(rental_price: rental_price) if rental_price
+    return where(buying_price: buying_price) if buying_price
+    return all
+  end
 
 end
