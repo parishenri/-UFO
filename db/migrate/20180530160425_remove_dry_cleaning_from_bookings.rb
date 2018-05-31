17df2744066cceeab3890c0dbdbaa26a81de65e6
Chris Morrow <chrismorrow@blueyonder.co.uk>
2018-05-31 10:33:04 +0100

    schema fixed

--- db/migrate/20180530160425_remove_dry_cleaning_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDryCleaningFromBookings < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :bookings, :dry_cleaning, :string
-  end
-end
--- db/migrate/20180530162634_remove_dates_from_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDatesFromItems < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :items, :start_date, :date
-    remove_column :items, :end_date, :date
-  end
-end
