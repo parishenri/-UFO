45cd63264af5d25a04531cef490387aad4c08ff1
Gonzalo Diaz <info@ventra.com.co>
2018-06-01 16:26:05 +0100

    added navbar features

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,7 @@ function loadDynamicBannerText() {
   const banner = document.getElementById('banner-typed-text')
   if (banner) {
     new Typed('#banner-typed-text', {
-      strings: ["Being naked is the #1 most sustainable option", "We are #2"],
+      strings: ["Being naked is the no. 1 most sustainable option", "We are no. 2"],
       typeSpeed: 50,
       loop: true
     });
