f1e66214b929e0421d098c9ca2444e3d3bd3dc12
Henri Paris <paris_henri@yahoo.fr>
2018-06-04 13:33:40 +0100

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ loadDynamicBannerText();
 // 	plugins: [new rangePlugin({ input: "#end_date"})]
 // });
 
-
+// show
 const div = document.getElementById('bookedDates')
 if (div) {
   const dates = JSON.parse(document.getElementById('bookedDates').dataset.dates);
@@ -27,16 +27,31 @@ if (div) {
   });
 }
 
-flatpickr("#available_start_date", {
- altInput: true,
- plugins: [new rangePlugin({ input: "#available_end_date"})]
-});
 
+const available_start_date = document.getElementById('available_start_date')
 
+if (available_start_date) {
+  flatpickr("#available_start_date", {
+   altInput: true,
+   plugins: [new rangePlugin({ input: "#available_end_date"})]
+  });
+}
 
-const datesForSearch = JSON.parse(document.getElementById('allDates').dataset.dates);
-flatpickr("#start_date", {
- altInput: true,
- disable: datesForSearch,
- plugins: [new rangePlugin({ input: "#end_date"})]
-});
+const start_date_search = document.getElementById('start_date_search')
+
+if (start_date_search) {
+  flatpickr("#start_date_search", {
+   altInput: true,
+   plugins: [new rangePlugin({ input: "#end_date_search"})]
+  });
+}
+
+const datesdiv = document.getElementById('allDates')
+if (datesdiv) {
+  const datesForSearch = JSON.parse(datesdiv.dataset.dates);
+  flatpickr("#start_date", {
+   altInput: true,
+   disable: datesForSearch,
+   plugins: [new rangePlugin({ input: "#end_date"})]
+  });
+}
