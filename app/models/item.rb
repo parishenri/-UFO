2c41d6d82f6242122436102e6fd99d6734d294fe
arthur maisonnier <arthurmaisonnier@gmail.com>
2018-06-08 14:42:40 +0100

    working delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :orders, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews,  dependent: :destroy
   validates :name, presence: true
