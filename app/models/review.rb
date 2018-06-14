d81fa5102630c41570fcdb93a6f92bb44a4d89ce
Gonzalo Diaz <info@ventra.com.co>
2018-06-14 09:23:00 +0100

    no changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  validates_format_of :content, with: /^(?:\b\w+\b[\s\r\n]*){5,50}$/, message: "Please write a review of minimum 6 words", multiline: true
+  validates :content, length: { minimum: 20, too_short: "%{count} characters is the minimum allowed" }
+  validates :content, length: { maximum: 90, too_long: "%{count} characters is the maxmimum allowed" }
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0,1,2,3,4,5] }
 end
